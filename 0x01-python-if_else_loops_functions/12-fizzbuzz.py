2db937b6f4823a927f38388af6c8daa8cef9621d
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-04 23:52:18 +0200

    end of mandatory

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+#12-fizzbuzz.py
+#Rugwiro G Parfait <rugwiroparfait003@gmail.com?
+
+def fizzbuzz():
+    """Print the number from 1 to 100 separated by a space.
+
+    for multiple of five, print Buzz instead of the number.
+    for multiple of three,print Fizz instead of the number.
+    for multiple of three, and five , print FizzBuzz instead of the number.
+    """
+    for number in range(1,101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz ",end="")
+        elif number % 3 == 0:
+            print("Fizz ", end="")
+        elif number % 5 == 0:
+            print("Buzz ", end ="")
+        else:
+            print("{} ".format(number), end ="")
+
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 # 8-uppercase.py
 
 def uppercase(str):
     """Print a string in uppercase."""
-for c in str:
-    if ord(c) >= 97 and ord(c) <= 122:
-        c = chr(ord(c) - 32)
-    print("{}".format(c), end="")
+    for c in str:
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+         print("{}".format(c), end="")
     print("")
+
+# Example usage
+uppercase("best")
+uppercase("Best School 98 Battery street")
+
