4ca06471bac4bc621195b1e5937d81c59a39480e
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-05 07:18:03 +0200

    pycodestyle

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#12-fizzbuzz.py
-#Rugwiro G Parfait <rugwiroparfait003@gmail.com?
+# 12-fizzbuzz.py
+# Rugwiro G Parfait <rugwiroparfait003@gmail.com?
 
 def fizzbuzz():
     """Print the number from 1 to 100 separated by a space.
@@ -9,13 +9,12 @@ def fizzbuzz():
     for multiple of three,print Fizz instead of the number.
     for multiple of three, and five , print FizzBuzz instead of the number.
     """
-    for number in range(1,101):
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("FizzBuzz ",end="")
+            print("FizzBuzz ", end="")
         elif number % 3 == 0:
             print("Fizz ", end="")
         elif number % 5 == 0:
-            print("Buzz ", end ="")
+            print("Buzz ", end="")
         else:
-            print("{} ".format(number), end ="")
-
+            print("{} ".format(number), end="")
