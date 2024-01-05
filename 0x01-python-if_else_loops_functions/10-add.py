505dadd1669844f9577ba45b4927886042660e31
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-05 07:35:48 +0200

    10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# 9-print_last_digit.py
+# 10-add.py
 
-def print_last_digit(number):
-    """Print the last digit of a number and return it."""
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
