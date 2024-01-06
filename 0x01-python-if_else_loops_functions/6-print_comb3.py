5f5d143ca199b79afa7bc682be2cd7fde28a88c1
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-06 02:13:58 +0200

    fith

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
-
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
-            print('{}{}'.format(i, j))
-        else:
-            print('{}{}, '.format(i, j), end="")
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print("Welcome to {}!".format(str1))
