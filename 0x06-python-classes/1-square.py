6b628dd7fa0f2a804b162f2dabf4707a2398ec18
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-01-25 02:32:02 +0200

    1 debug for pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+""" class Square that defines a square"""
+
 
 class Square:
-    """
-    Class that defines properties of square by: (based on 0-square.py).
-
-    Attributes:
-        size: size of a square (1 side).
-    """
+    """ class Square that defines a square"""
     def __init__(self, size):
-        """Creates new instances of square (1 side).
-
+        """initialize square
         Args:
-            size: size of the square.
+            size (int): size of the square
         """
-        self.__size = size
+        self.__size = size  #: size of the square
