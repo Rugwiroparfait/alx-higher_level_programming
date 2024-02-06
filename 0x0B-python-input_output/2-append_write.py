1fe684af7a5d42babd2c03dae882e0e106918c46
root <root@DESKTOP-DKOFTEF>
2024-02-07 01:55:29 +0200

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def append_write(filename="", text=""):
+    """Appends a string at the end of a text file (UTF8) and returns the number of characters added."""
+    with open(filename, "a", encoding="UTF-8") as f:
+        num_characters_added = f.write(text)
+    return num_characters_added
+
