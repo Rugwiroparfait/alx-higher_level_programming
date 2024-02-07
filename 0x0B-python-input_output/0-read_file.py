f0efa37904daff363ed248e8f9a025c0de201b66
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-02-07 02:17:05 +0200

    all tasks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""this module writes to a file"""
 
 
-def read_file(filename=""):
-    with open(filename, "r", encoding="UTF-8") as f:
-        print(f.read(), end="")
+def write_file(filename="", text=""):
+    """writes a string to a text file and returns
+    the number of characters written"""
+    with open(filename, encoding="utf-8", mode="w+") as f:
+        return f.write(text)
