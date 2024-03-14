719129ffcb45c32acd4ce70242780bf55deca940
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-03-14 11:26:38 -0700

    8-count_89.sql

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,10 @@
+-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;-- displays number of records with id of 89
+SELECT count(*) FROM first_table WHERE `id` = 89;
