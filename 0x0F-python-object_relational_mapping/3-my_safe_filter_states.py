320ab5eeb2d387fc62e1a4e8e7b8b4bbf182472b
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-04-14 13:58:36 -0700

    1 to 5th

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,37 +1,17 @@
-#!/usr/bin/bin/python3
-"""Module that lists all states from the hbtn databse"""
-import sys
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
-
-    # get the commandline arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv [2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    # Connect to the MySQL server
-    db = MySQLdb.connect (
-        host='localhost',port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-
-    )
-    #create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Prepare the SQL query with placeholders
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query with the state name as a parameter
-    cursor.execute(sql_query, (state_name,))
-
-    # Fetch all the rows
-    rows = cursor.fetchall()
-    # Print the rows
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    match = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name LIKE %s", (match, ))
+    rows = cur.fetchall()
     for row in rows:
         print(row)
+    cur.close()
+    db.close()
--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-
-"""Module that lists all states from th hbtn dabase"""
-
-import sys
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-    # Get MySQL credentials and states name from command-line arguments
-    # and connect to MySQL server
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
 
-    # Execute the SQL query to retrieve cities in the specifies state
-    query = ("SELECT * FROM `cities` as `c` \
-             INNER JOIN `states` as `s` \
-             ON `c`, `state_id` = `s`.`id` \
-             ORDER BY `c`.`id`")
-    c.execute (query)
-
-    # Fetch all rows and filters cities by the specified state
-    # and Print the cities separated by commas
-    print(",".join([ct[2]  for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("""SELECT cities.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id
+                WHERE states.name=%s""", (sys.argv[4],))
+    rows = cur.fetchall()
+    tmp = list(row[0] for row in rows)
+    print(*tmp, sep=", ")
+    cur.close()
+    db.close()
