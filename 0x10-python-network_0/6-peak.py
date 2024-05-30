f504321fd0ac644882ca3df7fadad83da49be62c
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-05-30 06:52:42 -0700

    6-peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,47 +1,25 @@
 #!/usr/bin/python3
-"""Module that finds a peak in a list of integers"""
+""" Finds Peak values """
 
 
 def find_peak(list_of_integers):
-    """function that finds a peak in a list of unsorted integers.
-
-    Args:
-        list_of_integers (list): a list of integers
-
-    Your algorithm must have the lowest complexity.
-    Note: there may be more than one peak in the list.
-
-    The most naive solution to this is to just go through each element
-    one-by-one and see if it's qualified as a peak. This solution will take
-    O(n) time complexity at the worst case and O(1) for space complexity
-    which is super for most algorithm problem. So, here comes the tricky
-    part, solve it with O(log(n)) time complexity!
-
-    Usually Binary Search is being used in sorted array(it could also mean
-    Bitonic array, or array that's sorted in some other ways), but this
-    one is a little bit different as we can't sort the array.
-
-    In Binary Search, we always check the middle value and see if it's
-    qualified to be a peak, if not then we change the start or end pointer
-    so that we get a new middle value.
-
-    Returns:
-        int: peak(s)
-    """
-    list_ = list_of_integers
-    # if there is no list of integers return None
-    if list_ == []:
+    """Find the peak"""
+    list_l = len(list_of_integers)
+    if list_l is 0:
         return None
-    length = len(list_)
+    peak = binary_search(list_of_integers, 0, list_l - 1)
+    return list_of_integers[peak]
 
-    start, end = 0, length - 1
-    while start < end:
-        mid = start + (end - start) // 2
-        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
-            return list_[mid]
-        if list_[mid - 1] > list_[mid + 1]:
-            end = mid
-        else:
-            start = mid + 1
-    return list_[start]
-    
+
+""" binary search algorithim """
+
+
+def binary_search(a, lo, hi):
+    """Recursive binary search of the peak"""
+    if lo >= hi:
+        return lo
+    mid = ((hi - lo) // 2) + lo
+    if a[mid] > a[mid + 1]:
+        return binary_search(a, lo, mid)
+    else:
+        return binary_search(a, mid + 1, hi)
