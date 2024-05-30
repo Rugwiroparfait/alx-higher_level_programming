32cd59c0d96489db0bb62bf6359193fbb4738199
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-05-30 06:13:00 -0700

    0-body_size.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+./4-header.sh 0.0.0.0:5000/route_5 ; echo ""#!/bin/bash
 # script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
 curl -s "$1" -H "X-School-User-Id: 98"
